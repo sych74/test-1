5bb5c092a97ddcf6c59d75c4e0648fc4a1deddfc
sych74 <sych74@gmail.com>
2020-05-07 17:15:05 +0300

    Update beforeinstall.js

--- beforeinstall.js
+++ beforeinstall.js
@@ -9,13 +9,20 @@ var resp = {
 
 resp.nodes.push({
   nodeType: "mariadb-dockerized",
-  tag: "10.4.12",
+  tag: ${settings.sqldb_tag:"10.4.12"},
   flexibleCloudlets: ${settings.db_flexibleCloudlets:16},
   fixedCloudlets: ${settings.db_fixedCloudlets:1},
   diskLimit: ${settings.db_diskLimit:10},
   nodeGroup: "sqldb",
   restartDelay: 5,
-  skipNodeEmails: true
+  skipNodeEmails: true,
+  displayName: db_cluster_name,
+  cluster: {
+    scheme: db_cluster,
+    db_user: ${globals.DB_USER},
+    db_pass: ${globals.DB_PASS},
+    is_proxysql: false
+  }
 });
 
 return resp;
